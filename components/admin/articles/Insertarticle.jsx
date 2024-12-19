f185e44e33d1e4eea448f7a8396793d574d0d1bd
salmenibnsalem <salmenibnsalem@gmail.com>
2024-12-19 14:43:23 +0100

    Commit new version

--- components/admin/articles/Insertarticle.jsx
+++ components/admin/articles/Insertarticle.jsx
@@ -1,8 +1,74 @@
+import { useState } from "react"
+import { Form,Col,Row } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
 
 const Insertarticle = () => {
+  const [article,setArticles]=useState({})
+  
+
   return (
-    <div>
-      Inserer un article
+    <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+      <h1>Inserer un article</h1>
+      <Form>
+        <Row>
+      <Form.Group as={Col} md='6' >
+        <Form.Label>Reference</Form.Label>
+        <Form.Control type="text" 
+        placeholder="Reference" 
+        value={article.reference} onChange={(e)=> setArticles({...article,reference:e.target.value})} />
+      </Form.Group>
+
+      <Form.Group as={Col} md='6'>
+        <Form.Label>Reference</Form.Label>
+        <Form.Control type="text" 
+        placeholder="Designation" 
+        value={article.designation} onChange={(e)=> setArticles({...article,designation:e.target.value})} />
+      </Form.Group>
+      </Row>
+
+      <Row>
+      <Form.Group as={Col} md='6' >
+        <Form.Label>Marque</Form.Label>
+        <Form.Control type="text" 
+        placeholder="Marque" 
+        value={article.marque} onChange={(e)=> setArticles({...article,marque:e.target.value})} />
+      </Form.Group>
+
+      <Form.Group as={Col} md='6'>
+        <Form.Label>Prix</Form.Label>
+        <Form.Control type="text" 
+        placeholder="Prix" 
+        value={article.prix} onChange={(e)=> setArticles({...article,prix:e.target.value})} />
+      </Form.Group>
+      </Row>
+
+      <Row>
+      <Form.Group as={Col} md='6' >
+        <Form.Label>Stock</Form.Label>
+        <Form.Control type="text" 
+        placeholder="Stock" 
+        value={article.qtestock} onChange={(e)=> setArticles({...article,qtestock:e.target.value})} />
+      </Form.Group>
+
+      <Form.Group as={Col} md='6'>
+        <Form.Label>Image</Form.Label>
+        <Form.Control type="text" 
+        placeholder="Image" 
+        value={article.imageart} onChange={(e)=> setArticles({...article,imageart:e.target.value})} />
+      </Form.Group>
+      </Row>
+      <Row>
+      <Form.Group as={Col} md='12'>
+        <Form.Label>Scategorie</Form.Label>
+        <Form.Control type="text" 
+        placeholder="S/categorie" 
+        value={article.scategorieID} onChange={(e)=> setArticles({...article,scategorieID:e.target.value})} />
+      </Form.Group>
+      </Row>
+    </Form>
+    <button className="btn btn-success btn-sm" onClick={(e)=>handleSave(e)}>Enregistre</button>
+    <Link to="/articles"><button className="btn btn-danger btn-sm">Delete</button></Link>
     </div>
   )
 }
--- components/admin/articles/Listarticle.jsx
+++ components/admin/articles/Listarticle.jsx
@@ -1,12 +1,17 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactLoading from 'react-loading';
+import { Link } from 'react-router-dom';
 
 const Listarticle = () => {
   const [articles,setArticles]=useState([])
+  const [isloading,setIsloading]=useState(true)
   const fetcharticles=async()=>{
     try{
-      const res= await axios.get("http://localhost:3001/api/article")
-      console.log(res)
+      const res= await axios.get("https://backend-ecommerce-one-bay.vercel.app/api/article")
+      setArticles(res.data)
+      setIsloading(false)
+      console.log(res.data)
     }
     catch(error){
         console.log(error)
@@ -14,11 +19,53 @@ const Listarticle = () => {
       }
 
   }
+  useEffect(()=>{
+    fetcharticles()
+  },[])
+  if (isloading){
+    return(
+      <>
+      <center>    <ReactLoading type="spin" color="red" height={300} width={200} /><h3>En cours de chargment..</h3>
+      </center></>
+    )
+  }
   return (
-    <div>
-     <h1>Liste article</h1>
-     <button onClick={()=>fetcharticles()}>Affiche</button>
 
+    <div>
+      <p>
+      <td><Link to="/articles/add"><button className='btn btn-success btn-sm'><i class="fa-solid fa-square-plus"></i> Nouveau</button></Link></td>
+      </p>
+    <center> <h1>Liste article</h1></center>
+    <table className='table table-stripes'>
+     <thead>
+     <tr>
+       <th>Reference</th>
+       <th>Designation</th>
+       <th>Marque</th>
+       <th>Stock</th>
+       <th>Prix</th>
+       <th>Image</th>
+       <th>Update</th>
+       <th>Delete</th>
+      </tr>
+     </thead>
+    <tbody>
+
+      {
+        articles.map((art,index)=>
+          <tr>
+            <td>{art.reference}</td>
+            <td>{art.designation}</td>
+            <td>{art.marque}</td>
+            <td>{art.qtestock}</td>
+            <td>{art.prix}</td>
+            <td><img src={art.imageart} width={100} height={100}/></td>
+            <td><button className='btn btn-arning btn-sm'><i class="fa-sharp fa-solid fa-pen-to-square"></i>Update</button></td>
+            <td><button className='btn btn-danger btn-sm'><i class="fa-sharp fa-solid fa-trash"></i>Delete</button></td>
+          </tr>
+      )}
+    </tbody>
+    </table>
     </div>
   )
 }
