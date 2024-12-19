e71426649bef6073f77b630a979396bdb7099926
salmenibnsalem <salmenibnsalem@gmail.com>
2024-12-19 11:46:15 +0100

    updated commit

--- components/admin/articles/Listarticle.jsx
+++ components/admin/articles/Listarticle.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Listarticle = () => {
+  const [articles,setArticles]=useState([])
+  const fetcharticles=async()=>{
+    try{
+      const res= await axios.get("http://localhost:3001/api/article")
+      console.log(res)
+    }
+    catch(error){
+        console.log(error)
+      
+      }
+
+  }
   return (
     <div>
-     Liste des article
+     <h1>Liste article</h1>
+     <button onClick={()=>fetcharticles()}>Affiche</button>
 
     </div>
   )
